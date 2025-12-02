1285e32a3b2a2334bbc179d9cdebfec50bc31447
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-02 13:32:22 +0530

    check for target in sorted array + palindrome

---
+++ arrays and strings/two pointer/check for target in sorted array.py
@@ -0,0 +1,15 @@
+def check_for_target(nums, target):
+    left=0
+    right = len(nums)-1
+
+    while left<right:
+        curr = nums[left]+nums[right]
+        if curr == target:
+            return True
+        if curr<target:
+            left+=1
+        else:
+            right-=1
+    
+    return False
+        
