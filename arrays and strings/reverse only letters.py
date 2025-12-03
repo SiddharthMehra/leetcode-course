7f99a7727756ee7e37e1528640ad92b22fef5816
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-03 22:35:25 +0530

    reverse only letters

---
+++ arrays and strings/reverse only letters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        ans=[]
+        j = len(ans)-1
+
+        for i,x in enumerate(s):
+            if x.isalpha():
+                while not s[j].isalpha():
+                    j-=1
+                ans.append(s[j])
+                j-=1
+            
+            else:
+                ans.append(x)
+        
+        return "".join(ans)
+
+        
