07d72d74b4d2c846a90312bc5248e05f97692573
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-05 19:01:11 +0530

    sort characters by frequency

---
+++ hashmap/sort characters by frequency.py
@@ -0,0 +1,22 @@
+# bucket sort
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if not s:
+            return s
+        
+        counts = Counter(s)
+        max_freq = max(counts.values())
+
+        buckets = [[] for _ in range(max_freq+1)]
+
+        for c, i in counts.items():
+            buckets[i].append(c)
+        
+        string_builder = []
+
+        for i in range(len(buckets)-1, 0, -1):
+            for c in buckets[i]:
+                string_builder.append(c*i)
+        
+        return "".join(string_builder)
