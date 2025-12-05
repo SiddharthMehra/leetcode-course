473f0d179e4a6b7885797e9789bacf8f9b63aee9
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-05 22:45:26 +0530

    isomorphic strings

---
+++ hashmap/isomorphic strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def transformString(self, s: str) -> bool:
+        index_mapping = {}
+        new_str = []
+
+        for i,c in enumerate(s):
+            if c not in index_mapping:
+                index_mapping[c]=i
+            
+            new_str.append(str(index_mapping[c]))
+        
+        return " ".join(new_str)
+            
+    def isIsomorphic(self, s:str, t:str)->bool:
+        return self.transformString(s) == self.transformString(t)
+       
