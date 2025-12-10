4b46d2b1ac862752fdc2a26719abf7f889b62e0f
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-11 00:07:46 +0530

    validate stack sequences

---
+++ stack/validate stack sequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+
+        stack = []
+        i=0
+
+        for n in pushed:
+            stack.append(n)
+            while stack and stack[-1] == popped[i]:
+                stack.pop()
+                i+=1
+        
+        return i==len(popped)
+
+                
+
