35cadca95d0e90064ed1b5b571d8bde2f4ff7f74
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-12-11 23:52:42 +0530

    sum of subarray ranges

---
+++ stack/sum of subarray ranges.py
@@ -0,0 +1,33 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+
+        n, ans = len(nums), 0
+        stack = []
+
+        #sum of all minimums
+        for right in range(n+1): # iterate till end to process all elements
+            while stack and (right==n or nums[stack[-1]]>=nums[right]):
+
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                ans -= nums[mid] * (mid-left) * (right-mid)
+            
+            stack.append(right)
+        
+        stack.clear()
+
+        for right in range(n+1):
+            while stack and (right==n or nums[stack[-1]]<=nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                ans +=nums[mid] * (mid-left) * (right-mid)
+            
+            stack.append(right)
+        
+        return ans
+
+
+             
+
+
+
