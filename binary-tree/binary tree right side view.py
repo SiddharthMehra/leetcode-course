8716f252fae3901231cfe5b5eb31eb7661be498d
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-12 15:52:32 +0530

    Create binary tree right side view.py

---
+++ binary-tree/binary tree right side view.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        if not root:
+            return []
+        
+        ans = []
+        q = deque([root])
+
+        while q:
+            current_length = len(q)
+            ans.append(q[-1].val) #append rightmost to the answer
+
+            for _ in range(current_length):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        
+        return ans
