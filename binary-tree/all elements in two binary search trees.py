31cfc8656b91456bf6c0270fb58a9184531138c6
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-16 17:56:27 +0530

    Create all elements in two binary search trees.py

---
+++ binary-tree/all elements in two binary search trees.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorder(self, root, currList):
+        if not root:
+            return
+        
+        self.inorder(root.left, currList)
+        currList.append(root.val)
+        self.inorder(root.right, currList)
+
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+
+        list1, list2, ans = deque(), deque(), []
+
+        self.inorder(root1, list1)
+        self.inorder(root2, list2)
+
+        #merge two sorted lists
+        while list1 and list2:
+            ans.append(list1.popleft() if list1[0]<list2[0] else list2.popleft())
+        
+        return ans + list(list1) + list(list2) # add remaining elements
+
+
+
+        
