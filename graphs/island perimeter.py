d604684bb74a30e098ea85ca51a407ce5436585b
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-16 21:48:34 +0530

    Create island perimeter.py

---
+++ graphs/island perimeter.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        result = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1:
+                    result+=4
+                
+                    #check up
+                    if r>0 and grid[r-1][c]==1:
+                        result-=2
+
+                    #check left
+                    if c>0 and grid[r][c-1]==1:
+                        result-=2
+        
+        return result
