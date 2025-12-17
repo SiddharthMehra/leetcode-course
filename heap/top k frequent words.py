7c8f9b57586d70bff6a61cc4faa0fbbf7548454f
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-17 23:45:51 +0530

    Create top k frequent words.py

---
+++ heap/top k frequent words.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        cnt = Counter(words)
+        heap = [(-freq, word) for word, freq in cnt.items()]
+        heapq.heapify(heap)
+
+        return [heappop(heap)[1] for _ in range(k)]
