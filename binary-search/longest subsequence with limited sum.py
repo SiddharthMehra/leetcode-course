f13dd898e73c48b655c243517a779433b642e21e
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-22 19:15:46 +0530

    Create longest subsequence with limited sum.py

---
+++ binary-search/longest subsequence with limited sum.py
@@ -0,0 +1,17 @@
+#sort nums + prefix sum + binary search
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            nums[i]+=nums[i-1]
+        
+        ans = []
+
+        for query in queries:
+            index = bisect.bisect_right(nums, query)
+            ans.append(index)
+        
+        return ans
