94079b7c13f0f7016db89cf7ad3155a33a163f25
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-25 14:13:11 +0530

    Create combinations.py

---
+++ backtracking/combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(curr, i):
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
+            
+            for j in range(i, n+1):
+                curr.append(j)
+                backtrack(curr, j+1)
+                curr.pop()
+        
+        ans = []
+        #1 to n 
+        backtrack([], 1)
+        return ans
+
+        
+
+
+            
