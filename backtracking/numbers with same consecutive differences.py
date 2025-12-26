d6df9ee8dce1a8a4f6204c5cecfaeb680b466861
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-26 14:34:45 +0530

    Create numbers with same consecutive differences.py

---
+++ backtracking/numbers with same consecutive differences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        if n==1:
+            return [i for i in range(10)]
+        
+        ans = []
+        
+        def dfs(n, num):
+            #n digit num constructed with k diff between digits
+            if n==0:
+                return ans.append(num)
+            
+            tail_digit = num%10
+            next_digits = set([tail_digit+k, tail_digit-k])
+
+            #consider all possible options
+            for next_digit in next_digits:
+                if 0<=next_digit<10:
+                    new_num = num*10 + next_digit
+                    dfs(n-1, new_num)
+        
+        #starting digit can be 1-9
+        for num in range(1, 10):
+            dfs(n-1, num)
+        
+        return list(ans)
+
+        
