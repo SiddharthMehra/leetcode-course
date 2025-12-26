b98933ead90a785d2dabdc5f764ddc921c470426
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-26 23:34:16 +0530

    Create permutations II.py

---
+++ backtracking/permutations II.py
@@ -0,0 +1,24 @@
+#use hashmap to count occurences of a number
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        results = []
+
+        def backtrack(comb, counter):
+            if len(comb) == len(nums):
+                results.append(comb[:])
+                return
+            
+            for num in counter:
+                if counter[num]>0:
+                    comb.append(num)
+                    counter[num]-=1
+
+                    backtrack(comb, counter)
+
+                    comb.pop()
+                    counter[num]+=1
+        
+        backtrack([], Counter(nums))
+        return results
+      
