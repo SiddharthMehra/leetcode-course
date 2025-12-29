062decdaf0578c0f2db97bf3be8a71faa7623b66
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-29 13:26:59 +0530

    Create nth tribonacci number.py

---
+++ DP/nth tribonacci number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        #first three tribonacci numbrs are 0,1,1
+        if n<3:
+            return 1 if n else 0
+        
+        a,b,c = 0,1,1
+
+        for _ in range(n-2):
+            a,b, c = b, c, a+b+c
+        
+        return c
