38c8d44f040c6232544f4d63885f1c3a7efa4806
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2025-12-31 00:26:40 +0530

    Create implement tree(trie).py

---
+++ Trie/implement tree(trie).py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.endOfWord = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            
+            node = node.children[c]
+        
+        node.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        
+        return node.endOfWord
+
+    #return if there is any word starting with the given prefix
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
