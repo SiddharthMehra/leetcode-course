546a77479b50c01e96f3777617970a175d63708a
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-01-14 17:41:28 +0530

    Create 4 sum.py

---
+++ arrays and strings/4 sum.py
@@ -0,0 +1,40 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res, quad = [], []
+        
+        #target and nums are both accessible within this function, but we only have target still it keeps changing
+        def kSum(k, start, target):
+            if k!=2:
+                for i in range(start, len(nums) - k +1):
+                    if i>start and nums[i] == nums[i-1]:
+                        continue
+                    #backtrack
+                    quad.append(nums[i])
+                    kSum(k-1, i+1, target - nums[i])
+                    quad.pop()
+                
+                return
+            
+            #base case, 2 sum
+            l, r = start, len(nums)-1
+            while l<r:
+                if nums[l]+nums[r]<target:
+                    l+=1
+                elif nums[l]+nums[r]>target:
+                    r-=1
+                else:
+                    res.append(quad + [nums[l], nums[r]])
+                    l+=1
+                    while l< r and nums[l] == nums[l-1]:
+                        l+=1
+
+        
+        kSum(4, 0, target)
+        return res
+
+                
+
+
+
+            
