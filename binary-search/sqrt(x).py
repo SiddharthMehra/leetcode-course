c6ee8904b6471be29d094a3749ac8f9b79801243
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-01-16 13:41:35 +0530

    Create sqrt(x).py

---
+++ binary-search/sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        #binary search
+        first, last = 1, x
+
+        while first<=last:
+            mid = (first+last)//2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                last = mid-1
+            else:
+                first = mid+1
+        
+        return last
