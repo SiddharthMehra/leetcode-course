e13662807b1677c3502f3c82d923c45d3aead399
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-01-21 01:11:24 +0530

    Create buildings with an ocean view.py

---
+++ arrays and strings/buildings with an ocean view.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        #iterate in reverse to print the building with an ocean view
+        res = [len(heights)-1]
+        for i in range(len(heights)-2, -1, -1):
+            if heights[i]>heights[res[-1]]:
+                res.append(i)
+        
+        return res[::-1]
+
+
+
+
+        
