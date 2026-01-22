8e73d44171af694ce739370374e3fc217faaed80
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-01-23 01:00:16 +0530

    Create find the celebrity.py

---
+++ graphs/find the celebrity.py
@@ -0,0 +1,24 @@
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        
+        #assume 0 to be the celebrity initially
+        candidate = 0
+
+        for i in range(1,n):
+            if knows(candidate, i):
+                candidate = i
+        
+        #verify candidate
+        for i in range(n):
+            if i==candidate:
+                continue
+            
+            if knows(candidate, i) or not knows(i, candidate):
+                return -1
+        
+        return candidate
+            
