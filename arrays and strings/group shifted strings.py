137f2af0c37f559a9472b107b4848faf4f82a314
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-04 21:57:07 +0530

    Create group shifted strings.py

---
+++ arrays and strings/group shifted strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+
+        #create hash value
+
+        def get_hash(string: str):
+            key = []
+            for a,b in zip(string, string[1:]):
+                key.append(chr((ord(b) - ord(a)) % 26 + ord('a')))
+            
+            return ''.join(key)
+        
+        groups = defaultdict(list)
+        for string in strings:
+            hash_key = get_hash(string)
+            groups[hash_key].append(string)
+        
+        return list(groups.values())
