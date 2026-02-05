5632a0115edea0181446ce7c1c5aaf3adc56ca1c
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-05 18:16:31 +0530

    Create maximum product subarray.py

---
+++ DP/maximum product subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        res = max(nums)
+        curr_min, curr_max = 1, 1
+        for n in nums:
+            if n == 0:
+                curr_min, curr_max = 1, 1
+                continue
+            
+            temp = curr_max
+            curr_max = max(n * curr_max, n * curr_min, n)
+            #use the current curr_max
+            curr_min = min(n * temp, n * curr_min, n)
+
+            res = max(res, curr_max, curr_min)
+        
+        return res
+
