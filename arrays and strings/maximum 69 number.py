caef7558f58c83695a9d0bfa311b2ba9fd2c681a
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-05 18:01:36 +0530

    Create maximum 69 number.py

---
+++ arrays and strings/maximum 69 number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+
+        curr_digit = 0
+        index_first_six = -1
+        num_copy = num
+
+        while num_copy:
+            if num_copy%10==6:
+                index_first_six = curr_digit
+            
+            num_copy//=10
+            curr_digit+=1
+        
+        return num if index_first_six == -1 else num + 3 * 10 ** index_first_six
+        
