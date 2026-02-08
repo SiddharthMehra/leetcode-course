5df8df7dea794d85cf100575cc0ec17118319b9e
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-09 01:27:09 +0530

    Create word break II.py

---
+++ DP/word break II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+
+        dp = {}
+        def word_break(s):
+            if s in dp:
+                return dp[s]
+            
+            result = []
+            for w in wordDict:
+                if s[:len(w)] == w:
+                    if len(w) == len(s):
+                        result.append(w)
+                    else:
+                        temp = word_break(s[len(w):])
+                        for t in temp:
+                            result.append(w + " " + t)
+            dp[s] = result
+            return result
+        
+        return word_break(s)
+
