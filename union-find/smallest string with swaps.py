11e653260c4b88635c3c9cd7d14a266caad2f9b0
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-10 22:37:32 +0530

    Create smallest string with swaps.py

---
+++ union-find/smallest string with swaps.py
@@ -0,0 +1,36 @@
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+
+        class UF:
+
+            def __init__(self, n):
+                self.parent = list(range(n))
+            
+            def union(self, x, y):
+                self.parent[self.find(x)] = self.find(y)
+            
+            def find(self, x):
+                if x!=self.parent[x]:
+                    self.parent[x] = self.find(self.parent[x])
+                
+                return self.parent[x]
+            
+        uf, res, m = UF(len(s)), [], defaultdict(list)
+
+        for x,y in pairs:
+            uf.union(x, y)
+        
+        for i in range(len(s)):
+            m[uf.find(i)].append(s[i])
+        
+        for comp_id in m.keys():
+            m[comp_id].sort(reverse = True)
+        
+        for i in range(len(s)):
+            res.append(m[uf.find(i)].pop())
+        
+        return ''.join(res)
+
+        
+        
+    
