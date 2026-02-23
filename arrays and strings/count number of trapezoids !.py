5b9afa6c6876dcbfeba2e5b1f33a35f0015bb477
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-23 18:31:05 +0530

    Create count number of trapezoids !.py

---
+++ arrays and strings/count number of trapezoids !.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+
+        point_num = defaultdict(int)
+        mod = 10**9+7
+        ans, total_sum = 0, 0
+        #count the number of points at the same y coordinate
+        for point in points:
+            point_num[point[1]]+=1
+        
+        for p_num in point_num.values():
+            #n choose 2
+            edge = p_num * (p_num-1)//2
+            ans = (ans + edge*total_sum) % mod
+            total_sum = (total_sum + edge) % mod
+        
+        return ans
+        
+        
