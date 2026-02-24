493ce2406140948999dfce46ef1994dbb3fd1ca0
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-24 18:25:21 +0530

    Create vowels game in a string.py

---
+++ arrays and strings/vowels game in a string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+
+        for ch in 'aeiou':
+            if ch in s:
+                return True
+        
+        return False
+        
