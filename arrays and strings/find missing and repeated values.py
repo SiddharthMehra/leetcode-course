f9cfda77617a6087ee362c66c19c171fbc6154c7
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-02-27 17:51:43 +0530

    Create find missing and repeated values.py

---
+++ arrays and strings/find missing and repeated values.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+
+        n = len(grid)
+        total = n*n
+
+        sum_val = sum(num for row in grid for num in row)
+        sqr_sum = sum(num * num for row in grid for num in row)
+
+        sum_diff = sum_val - total * (total+1)//2
+        sqr_diff = sqr_sum - total * (total+1) * (2 * total + 1)//6
+
+        #sum diff = x-y, sqr_diff = x2- y2
+        repeat = (sqr_diff//sum_diff + sum_diff)//2
+        missing = (sqr_diff//sum_diff - sum_diff)//2
+
+        return [repeat, missing]
+        
