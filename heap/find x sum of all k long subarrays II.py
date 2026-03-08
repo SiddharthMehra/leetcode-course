5894baf8acd2224b2b976445ebb9ccfba08a5570
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-08 17:13:36 +0530

    Create find x sum of all k long subarrays II.py

---
+++ heap/find x sum of all k long subarrays II.py
@@ -0,0 +1,88 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+
+        freq = defaultdict(int)
+        top, rest = SortedList(), SortedList()
+
+        top_sum = 0
+        ans=[]
+
+        def add(num):
+
+            nonlocal top_sum
+
+            old = (freq[num], num)
+            if old in top:
+                top.remove(old)
+                top_sum-=old[0] * old[1]
+            
+            elif old in rest:
+                rest.remove(old)
+            
+            freq[num]+=1
+            new = (freq[num], num)
+            rest.add(new)
+            balance()
+        
+        def remove(num):
+            nonlocal top_sum
+            old = (freq[num], num)
+            if old in top:
+                top.remove(old)
+                top_sum-=old[0] * old[1]
+            else:
+                rest.remove(old)
+            
+            freq[num]-=1
+            if freq[num]>0:
+                rest.add((freq[num], num))
+            else:
+                del freq[num]
+            balance()
+        
+        def balance():
+
+            nonlocal top_sum
+
+            #pop the largest from top and give it to rest
+            while len(top)<x and rest:
+                f, v = rest.pop()
+                top.add((f, v))
+                top_sum+=f*v
+            
+            #pop the smallest
+            while len(top)>x:
+                f, v = top.pop(0)
+                top_sum-=f*v
+                rest.add((f, v))
+            
+            if rest and top and rest[-1]>top[0]:
+                f1, v1 = rest.pop()
+                f2, v2 = top.pop(0)
+
+                top_sum+=f1*v1
+                top_sum-=f2*v2
+
+                rest.add((f2, v2))
+                top.add((f1, v1))
+
+        res = []
+
+        for i in range(k):
+            add(nums[i])
+        
+        ans.append(top_sum)
+
+        for i in range(k, len(nums)):
+            remove(nums[i-k])
+            add(nums[i])
+            ans.append(top_sum)
+        
+        return ans
+
+                 
+
+
+            
+
+        
