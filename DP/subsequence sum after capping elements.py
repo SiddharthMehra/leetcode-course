500289a46edfcabb21536756a3652567f59edd92
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-11 16:47:36 +0530

    Create subsequence sum after capping elements.py

---
+++ DP/subsequence sum after capping elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsequenceSumAfterCapping(self, nums: List[int], k: int) -> List[bool]:
+
+        nums.sort()
+        n = len(nums)
+        res = [False] * n
+        dp = 1
+        mask = (1<<k+1) - 1
+
+        i=0 
+
+        for x in range(1, n+1):
+            while i<n and nums[i]<=x:
+                dp |=(dp<<nums[i]) & mask
+                i+=1
+            v = max(k % x, k - (n-i) * x)
+            for j in range(v, k+1, x):
+                if dp & (1<<j):
+                    res[x-1] = True
+                    break
+        
+        return res
+        
