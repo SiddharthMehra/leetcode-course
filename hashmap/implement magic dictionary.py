e46720d64af7fd9b19f53c6be9697a36d6cf916c
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-18 00:09:01 +0530

    Create implement magic dictionary.py

---
+++ hashmap/implement magic dictionary.py
@@ -0,0 +1,43 @@
+class MagicDictionary:
+
+    def __init__(self):
+
+        self.myDict = defaultdict(set)
+
+    def buildDict(self, dictionary: List[str]) -> None:
+
+        for word in dictionary:
+            n = len(word)
+            self.myDict[n].add(word)
+        
+    def search(self, searchWord: str) -> bool:
+
+        n = len(searchWord)
+        if n not in self.myDict:
+            return False
+        
+        def diff(word1, word2):
+            count = 0
+
+            for i in range(len(word1)):
+                if word1[i]!=word2[i]:
+                    count+=1
+                if count>1:
+                    return count
+            
+            return count
+        
+        for word in self.myDict[n]:
+            if diff(word, searchWord) == 1:
+                return True
+        
+        return False
+        
+
+        
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
