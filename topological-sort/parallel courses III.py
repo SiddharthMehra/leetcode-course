6f144ce3b5f8b212e72ba5bae908e58ea3b90842
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-18 02:24:11 +0530

    Create parallel courses III.py

---
+++ topological-sort/parallel courses III.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+
+        graph = defaultdict(list)
+        indegree = [0] * n
+
+        for (x,y) in relations:
+            graph[x-1].append(y-1)
+            indegree[y-1]+=1
+        
+        q = deque()
+        max_time = [0] * n
+
+        for node in range(n):
+            if indegree[node] == 0:
+                q.append(node)
+                max_time[node] = time[node]
+        
+        while q:
+            node = q.popleft()
+            for neighbor in graph[node]:
+                max_time[neighbor] = max(max_time[neighbor], max_time[node] + time[neighbor])
+                indegree[neighbor]-=1
+                if indegree[neighbor]==0:
+                    q.append(neighbor)
+    
+        return max(max_time)
