9fc825cd0fdfd30a660fad6934cec37eb0ed9869
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-18 01:15:00 +0530

    Create count paths that can form palindrome in a tree.py

---
+++ bit manipulation/count paths that can form palindrome in a tree.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def countPalindromePaths(self, parent, s):
+
+        @cache
+        def f(i):
+            return f(parent[i]) ^ (1<<(ord(s[i]) - ord('a'))) if i else 0
+        
+        count = Counter()
+        res = 0
+
+        for i in range(len(parent)):
+            v = f(i)
+            res+=count[v] + sum(count[v ^ (1<<j)] for j in range(26))
+            count[v]+=1
+        
+        return res
+        
