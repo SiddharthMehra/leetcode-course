33f1c7856d8dce9bb4222155ec801a5f52950bca
SiddharthMehra <59006456+SiddharthMehra@users.noreply.github.com>
2026-03-20 18:42:32 +0530

    Create missing element in sorted array.py

---
+++ binary-search/missing element in sorted array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+
+        n = len(nums)
+        left, right = 0, n-1
+
+        while left<right:
+            
+            #equivalent to ceil(left+right/2)
+            mid = right - (right-left)//2
+            if (nums[mid] - nums[0]) - mid<k:
+                left = mid
+            else:
+                right = mid-1
+        
+        return nums[0] + k + left
